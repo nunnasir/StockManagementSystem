d85d4feb0ea8305b164d90f39350e9f34aa379a0
nasir uddin <nunnasir@gmail.com>
2018-08-08 13:10:34 +0600

    Update Date Search

--- StockManagementSystem/StockManagementSystem/DAL/DateSearchRepository.cs
+++ StockManagementSystem/StockManagementSystem/DAL/DateSearchRepository.cs
@@ -17,7 +17,8 @@ namespace StockManagementSystem.DAL
         public DataTable LoadDateSearchItem(DateSearch dateSearch)
         {
             SqlConnection conn = new SqlConnection(connection.connectionDb);
-            string query = @"SELECT ite.Name ItemName, sto.Quantity Quantity FROM StockOut sto INNER JOIN Items ite ON sto.ItemId = ite.Id WHERE Date BETWEEN '" + dateSearch.FromDate + "' AND '" + dateSearch.ToDate + "' AND MediumId = 1 ORDER BY sto.Id DESC";
+            //string query = @"SELECT ite.Name ItemName, sto.Quantity Quantity FROM StockOut sto INNER JOIN Items ite ON sto.ItemId = ite.Id WHERE Date BETWEEN '" + dateSearch.FromDate + "' AND '" + dateSearch.ToDate + "' AND MediumId = 1 ORDER BY sto.Id DESC";
+            string query = @"SELECT Name ItemName, SUM(Quantity) Quantity from StockoutDetail WHERE Date Between '" + dateSearch.FromDate + "' AND '" + dateSearch.ToDate + "' group by Name, Medium Having  Medium = 'Sell' ORDER BY Quantity DESC";
             SqlCommand command = new SqlCommand(query, conn);
             conn.Open();
             SqlDataReader sd = command.ExecuteReader();
